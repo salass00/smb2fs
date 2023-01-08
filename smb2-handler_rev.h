ba56869bd2f85666ddbb676ea11a76a5b57ba2cb
Fredrik Wikstrom <fredrik@a500.org>
2023-01-08 18:47:13 +0200

    Bumped version to 53.3 and updated the releasenotes.

--- smb2-handler_rev.h
+++ smb2-handler_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		53
-#define REVISION	2
-#define DATE		"29.12.2022"
-#define VERS		"smb2-handler 53.2"
-#define VSTRING		"smb2-handler 53.2 (29.12.2022)\r\n"
-#define VERSTAG		"\0$VER: smb2-handler 53.2 (29.12.2022)"
+#define REVISION	3
+#define DATE		"8.1.2023"
+#define VERS		"smb2-handler 53.3"
+#define VSTRING		"smb2-handler 53.3 (8.1.2023)\r\n"
+#define VERSTAG		"\0$VER: smb2-handler 53.3 (8.1.2023)"
