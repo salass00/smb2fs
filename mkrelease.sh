f87fe74b58d6d9fe4a6c01892804d1f3a9acbd1b
Fredrik Wikstrom <fredrik@a500.org>
2023-01-18 14:51:25 +0200

    Build both 68000 and 68020 binaries and add to OS3 release archive.

--- mkrelease.sh
+++ mkrelease.sh
@@ -5,10 +5,11 @@
 
 HOST="${1:-m68k-amigaos}"
 
-make clean
 if [ "$HOST" = "m68k-amigaos" ]; then
+  make -f makefile.os3 clean
   make -f makefile.os3 all
 else
+  make -f makefile.aros clean
   make -f makefile.aros all
 fi;
 
@@ -21,14 +22,16 @@ mkdir -p ${DESTDIR}/smb2fs/L
 if [ "$HOST" = "m68k-amigaos" ]; then
   cp -p Install-OS3 ${DESTDIR}/smb2fs/Install
   cp -p README-OS3 ${DESTDIR}/smb2fs/README
+  cp -p bin-000/smb2-handler ${DESTDIR}/smb2fs/L/smb2-handler.000
+  cp -p bin-020/smb2-handler ${DESTDIR}/smb2fs/L/smb2-handler.020
 else
   cp -p Install-AROS ${DESTDIR}/smb2fs/Install
   cp -p README-AROS ${DESTDIR}/smb2fs/README
+  cp -p smb2-handler ${DESTDIR}/smb2fs/L
 fi;
 cp -p COPYING ${DESTDIR}/smb2fs
 cp -p ${LIBSMB2}/LICENCE-LGPL-2.1.txt ${DESTDIR}/smb2fs/LICENCE-libsmb2
 cp -p releasenotes ${DESTDIR}/smb2fs
-cp -p smb2-handler ${DESTDIR}/smb2fs/L
 
 cp -p icons-os3/def_drawer.info ${DESTDIR}/smb2fs.info
 cp -p icons-os3/def_install.info ${DESTDIR}/smb2fs/Install.info
