9c469452af0f89df52eae442787b83bf60bf6cf9
Fredrik Wikstrom <fredrik@a500.org>
2025-06-19 15:32:44 +0300

    Bumped version to 53.10.

--- smb2-handler_rev.h
+++ smb2-handler_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		53
-#define REVISION	9
-#define DATE		"12.6.2025"
-#define VERS		"smb2-handler 53.9"
-#define VSTRING		"smb2-handler 53.9 (12.6.2025)\r\n"
-#define VERSTAG		"\0$VER: smb2-handler 53.9 (12.6.2025)"
+#define REVISION	10
+#define DATE		"19.6.2025"
+#define VERS		"smb2-handler 53.10"
+#define VSTRING		"smb2-handler 53.10 (19.6.2025)\r\n"
+#define VERSTAG		"\0$VER: smb2-handler 53.10 (19.6.2025)"
